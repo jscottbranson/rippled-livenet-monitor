e49f31e1f36cb3708afea3e61e4221fa6774421a
crypticrabbit <the@rabbitkick.club>
2023-01-24 10:48:06 -0500

    Support multiple Discord channels.

--- README.md
+++ README.md
@@ -14,16 +14,16 @@ As this tool is used to monitor the live network, it is not really useful for mo
 
 ## Warning
 This is an early stage project, so expect problems. For example, asyncio does not clean up neatly when exiting using a keyboard interrupt.
-This monitoring tool is not yet intended for use in production environments (however, it does tend to be pretty stable).
+This monitoring tool is not intended for solo use in a production environment - please use redundant monitoring for mission-critical infrastructure.
 
-As written, this code will produce errors with asyncio in Python 3.9. The code is tested and works with 3.10 and 3.11.
+Be cautious when using webhooks, as messages may be lost due to rate limiting or other factors.
 
 ## To-Do
 1. Check fields returned by 'server' subscription and update tables accordingly (additional fields are currently logged at the warning level).
 2. Attempt to reconnect to servers that stop sending new last_ledger index numbers (and send an admin warning if all servers stop advancing).
 3. Integrate with sqlite DBs to retrieve server and contact information mappings.
 4. Consider multithreading the websocket connections and output processor.
-5. Consider integrating websocket connection information into the stock server tables.
+5. Retry sending messages that fail
 6. Support Discord, Slack, Mattermost, and email notifications.
 
 ## Notifications
@@ -37,12 +37,13 @@ If enabled in `settings.py`, notifications will be sent at the following times:
 At this time, Twilio notifications will not be retried if the monitoring server is unable to reach the Twilio API. This functionality can be easily integrated in the future by passing the messages back into the notification_queue.
 
 ## Running the bot
+As written, this code will produce errors with asyncio in Python 3.9. The code is tested and works with 3.10 and 3.11.
 1. `git clone https://github.com/crypticrabbit/rippled_monitor.git`
 2. `cd rippled_monitor`
 3. `pip install -r requirements.txt`
 4. `cp settings_ex.py settings.py`
 5. Adjust `settings.py` as needed
-6. (optional) Save notification credentials as env variables.
+6. (optional) Save Twilio notification credentials as env variables.
 7. `python3 main.py`
 
 ## Updating
--- notifications/notify_discord.py
+++ notifications/notify_discord.py
@@ -16,28 +16,33 @@ async def discord_post(settings, notification):
     :param settings: Config file
     :param dict notification: Recipient information and message keys.
     '''
+    responses = []
     message = {'content': str(notification.get('message'))}
     discord_settings = notification.get('server').get('notifications').get('discord')
-    discord_url = f"{settings.DISCORD_WEBHOOK_URL}{discord_settings.get('discord_id')}/{discord_settings.get('discord_token')}"
-
-    logging.info(f"Preparing to send Discord message: '{message}'.")
 
     if discord_settings and message:
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.post(
-                    discord_url,
-                    headers={'Content-Type': 'application/json'},
-                    json=message,
-                ) as response:
-                    return response
-        except (
-            ValueError,
-            OSError,
-            socket.gaierror,
-        ) as error:
-            logging.error(f"Error sending Discord message: '{error}'.")
-            # It probably makes sense to retry sending the message here.
+        for server in discord_settings.get('discord_servers'):
+            discord_url = f"{settings.DISCORD_WEBHOOK_URL}{server.get('discord_id')}/{server.get('discord_token')}"
+
+            logging.info(f"Preparing to send Discord message: '{message}'.")
+
+            try:
+                async with aiohttp.ClientSession() as session:
+                    async with session.post(
+                        discord_url,
+                        headers={'Content-Type': 'application/json'},
+                        json=message,
+                    ) as response:
+                        responses.append(response)
+            except (
+                ValueError,
+                OSError,
+                socket.gaierror,
+            ) as error:
+                logging.error(f"Error sending Discord message: '{error}'.")
+                # It probably makes sense to retry sending the message here.
+
+        return responses
 
 async def rate_limit(settings, notification, response):
     '''
@@ -68,7 +73,8 @@ async def send_discord(settings, notification):
     :param dict notification: Recipient information and message keys
     '''
     if settings.SEND_DISCORD is True:
-        server_response = await discord_post(settings, notification)
-        await discord_response(settings, notification, server_response)
+        server_responses = await discord_post(settings, notification)
+        for response in server_responses:
+            await discord_response(settings, notification, response)
     else:
         logging.warning(f"Discord notifications are disabled in settings. Ignoring message: '{notification}'.")
